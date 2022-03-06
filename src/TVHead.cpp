bedefc7f139d445a5bb8e9be1ff9f697ebb7ab37
DayOS <43686351+Day-OS@users.noreply.github.com>
2022-03-06 18:43:34 -0300

    Fix mirroring rows

--- src/TVHead.cpp
+++ src/TVHead.cpp
@@ -3,12 +3,14 @@
 #include "data.c"
 #include "string.h"
 #include "iostream"
-#include "VTNCRW/src/lib.h"
-#define LED_LENGTH 20
+#include "VTNCRW/src/vtncrw.h"
+
+#define width 17
+#define height 11
 
 int main() {
     VTNCRW VTNCLib;
-    std::vector<unsigned char> fileToBeLoaded(prototipo_vtnc, prototipo_vtnc + prototipo_vtnc_size);
+    std::vector<unsigned char> fileToBeLoaded(amogus_vtnc, amogus_vtnc + amogus_vtnc_size);
     VTNCFile file = VTNCLib.read(fileToBeLoaded);
 
     stdio_init_all();
@@ -17,38 +19,42 @@ int main() {
     gpio_set_dir(LED_PIN, GPIO_OUT);
     gpio_put(LED_PIN, 1);
 
-    PicoLed::PicoLedController ledStrip = PicoLed::addLeds<PicoLed::WS2812B>(pio0, 0, 0, LED_LENGTH, PicoLed::FORMAT_GRB);
+    PicoLed::PicoLedController ledStrip = PicoLed::addLeds<PicoLed::WS2812B>(pio0, 0, 0, width*height, PicoLed::FORMAT_GRB);
     ledStrip.setBrightness(64);
-    
 
-
+    bool inverter = false;
+    int xres = file.layersResolution[0].x;
+    int yres = file.layersResolution[0].y;
     while (true) {
 
         for (size_t frame = 0; frame < file.framesQuantity; frame++)
         {
-            for (size_t i = 0; i < file.layersResolution[0].x * file.layersResolution[0].y; i++)
+            for (size_t y = 0; y < yres; y++)
             {
-                RGB COLOR = file.Colors[file.Layers->framesArray[frame].Pixels[i]];
-                ledStrip.setPixelColor(i,  PicoLed::RGB(COLOR.R, COLOR.G, COLOR.B));
-                
-
+                for (size_t x = 0; x < xres; x++)
+                {
+                    int linesoffset = y*xres;
+                    int currentIndex = !inverter? (xres - x) + linesoffset - 1: x + linesoffset;
+                    std::cout << y <<": " << currentIndex << " | " <<(x + linesoffset) << std::endl;
+                    RGBA COLOR = file.Colors[file.Layers->framesArray[frame].Pixels[currentIndex]];
+                    ledStrip.setPixelColor(x + linesoffset,  PicoLed::RGB(COLOR.R, COLOR.G, COLOR.B));
+                }
+                inverter = !inverter;
             }
+            inverter = false;
         ledStrip.show();
-        sleep_ms(100);
-        //file.Layers->framesArray->msDuration
+        sleep_ms(file.Layers[0].framesArray[frame].msDuration);
         }
-
-        printf("seeeeeeeeeeeeeeeekkusu\n");
         
 
-        std::cout << std::endl << "FILE: Is .VTNC? R: " << std::boolalpha <<  file.isFile;
-        std::cout << std::endl << "FILE: Layers Quantity: " << int(file.layersQuantity);
-        //std::cout << std::endl << "FILE: Layer Keys: " << int(file.Layers[0].layerKey);
-        std::cout << std::endl << "FILE: Layers Resolution: "<< std::dec << file.layersResolution[0].x << "x" << file.layersResolution[0].y;
-        std::cout << std::endl << "FILE: Colors Quantity: " << int(file.colorsQuantity);
-        std::cout << std::endl << "FILE: Colors: " << std::hex << int(file.Colors[1].R) << "|" << int(file.Colors[1].G) << "|" << int(file.Colors[1].B);
-        std::cout << std::endl << "FILE: Frames Quantity: " << int(file.framesQuantity);
-        std::cout << std::endl << "FILE: Frames Array: " << int(file.Layers->framesArray->Pixels[0]);
+        // std::cout << std::endl << "FILE: Is .VTNC? R: " << std::boolalpha <<  file.isFile;
+        // std::cout << std::endl << "FILE: Layers Quantity: " << int(file.layersQuantity);
+        // //std::cout << std::endl << "FILE: Layer Keys: " << int(file.Layers[0].layerKey);
+        // std::cout << std::endl << "FILE: Layers Resolution: "<< std::dec << file.layersResolution[0].x << "x" << file.layersResolution[0].y;
+        // std::cout << std::endl << "FILE: Colors Quantity: " << int(file.colorsQuantity);
+        // std::cout << std::endl << "FILE: Colors: " << std::hex << int(file.Colors[1].R) << "|" << int(file.Colors[1].G) << "|" << int(file.Colors[1].B);
+        // std::cout << std::endl << "FILE: Frames Quantity: " << int(file.framesQuantity);
+        // std::cout << std::endl << "FILE: Frames Array: " << int(file.Layers->framesArray->Pixels[0]);
         
         //sleep_ms(1000);
     }
